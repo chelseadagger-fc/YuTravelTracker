e8cf94304b801ced5791d73af15f09a6eae0a916
da.aldridge <da.aldridge@proton.me>
2023-11-04 19:04:41 -0400

    countries from database query are highlighted on svg map

--- index.js
+++ index.js
@@ -13,46 +13,38 @@ const db = new pg.Client({
   port: 5432,
 })
 
-let visitedCountries;
-let numCountries;
+let query;
+let countries = [];
 
-db.query("SELECT country_code FROM visited_countries", (err, res) => {
-  db.connect();
-  if (err) {
-    console.error("Error executing query.", err.stack);
-  } else {
-    visitedCountries = res.rows;
-  }
-  db.end();
-})
-
-db.query("SELECT COUNT(*) FROM visited_countries", (err, res) => {
-  db.connect();
-  if (err) {
-    console.error("Error executing query.", err.stack);
-  } else {
-    numCountries = res.rows;
-  }
-  db.end();
-})
+// db.query("SELECT COUNT(*) FROM visited_countries", (err, res) => {
+//   db.connect();
+//   if (err) {
+//     console.error("Error executing query.", err.stack);
+//   } else {
+//     total = res.rows;
+//   }
+//   db.end();
+// })
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  db.connect();
 
-  res.render("index.ejs", { numCountries : total })
+  query = await db.query("SELECT country_code FROM visited_countries");
+  
+  let countries = [];
+
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+
+  res.render("index.ejs", { countries: countries, total: countries.length });
+
+  db.end();
 });
-
-
-
-
-
-
-
-
-
 
 
 app.listen(port, () => {
